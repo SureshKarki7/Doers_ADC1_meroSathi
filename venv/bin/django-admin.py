0e3829d562247f63a62d6121d9d5327c3f7e92bd
SasmiGurung <np03a180017@heraldcollege.edu.np>
2020-01-09 00:54:10 +0545

    Django Project initialized

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sasmi/PycharmProjects/Doers_ADC1_meroSathi/Doers_ADC1_meroSathi/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
