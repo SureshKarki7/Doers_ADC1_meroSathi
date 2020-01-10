5ba1a1bc373c99248fa3670a85a56c863d564578
Suresh Karki <46915216+SureshKarki7@users.noreply.github.com>
2020-01-11 03:27:08 +0545

    Add files via upload

--- authApp/views.py
+++ authApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import UserRegisterform
 # Create your views here.
+def register(request):
+    if request.method=='POST':
+        form= UserRegisterform(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your account has been succesfully created')
+            return redirect('home')
+    else:
+        form= UserRegisterform()
+
+    return render(request, 'authApp/register.html', {'form': form})
